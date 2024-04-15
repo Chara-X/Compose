326a674f72567dd7db449954da22c059ed9e2974
Chara-X <1719552189@qq.com>
2024-04-16 01:10:24 +0800

    Update Comments

--- Compose.WpfApp/Components/MainPage.cs
+++ Compose.WpfApp/Components/MainPage.cs
@@ -6,7 +6,7 @@ namespace Compose.WpfApp.Components;
 
 internal class MainPage : IComposable
 {
-    // 级联样式
+    // Cascade Style
     private readonly StackPanel _stackPanel = new() { Resources = new ResourceDictionary { Source = new Uri("Resources/Styles.xaml", UriKind.Relative) } };
 
     private readonly Counter _counter = new();
@@ -19,10 +19,10 @@ internal class MainPage : IComposable
             x.SetChildren(
                 new TextBlock { Text = "Hello, world!", Style = (Style)_stackPanel.Resources["Heading"]! },
                 new TextBlock { Text = "Welcome to your new app." },
-                // 嵌套组件
+                // Child component
                 _counter.Compose(_value, OnCounterValueChanged));
         }));
-        // 条件渲染
+        // Condition render
         for (var i = 0; i < _value; i++) _stackPanel.Children.Add(new TextBlock { Text = "Item：" + i });
         return _stackPanel;
     }
